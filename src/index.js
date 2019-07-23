de9af011f795e04cc2a6ed9f3221534e571f15ab
Pedro Capriles <caprilespe@outlook.com>
2019-07-23 00:11:02 -0400

    Learning the different between use JSX and React.createElement and choose the best of then.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1> Hello, Platzi Badges from React</h1>;
+
+
+// const jsx = <h1> Hello, Platzi Badges from React</h1>;
+
+// const element = React.createElement('a',{href:'https://platzi.com'},'Ir a Platzi');
+const name ='Pedro Capriles';
+
+// const element = React.createElement('h1',{},`Hola, soy ${name}`);
+
+// const jsx =<h1> Hola soy, {name}</h1>;
+
+// const jsx =<h1> Hola soy, {name}</h1>;
+
+const jsx=(
+    <div>
+        <h1>
+            Hola, soy {name}
+        </h1>
+        <p>Soy Ingeniero de Sistemas.</p>
+    </div>
+);
+
+const element = React.createElement(
+    'div',{},
+    React.createElement('h1',{},`Hola, soy ${name}`),
+    React.createElement('p',{},'Soy Ingeniero de Sistemas'));
 
 const container = document.getElementById('app');
 
 //ReactDOM.render(__que se quiere renderizar__,___donde se quiere renderizar____)
 
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
