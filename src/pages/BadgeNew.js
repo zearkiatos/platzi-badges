b19cbb666da8b257e6c52aeba9c9e9996bc40bd2
Pedro Capriles <caprilespe@outlook.com>
2019-10-26 22:11:33 -0300

    Linked events

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import '../pages/styles/badgeNew/BadgeNew.css';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 import header from '../assets/images/badge-header.svg';
 class BadgeNew extends React.Component {
     render() {
@@ -13,8 +14,11 @@ class BadgeNew extends React.Component {
                 </div>
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-                        <Badge firstName="Pedro" lastName="Capriles" jobTitle="FullStack Developer" twitter="@zearkiatos" avatarUrl="https://s.gravatar.com/avatar/db9770705c9ea98b2185ea09179d588f?s=80" />
+                        <div className="col-6">
+                            <Badge firstName="Pedro" lastName="Capriles" jobTitle="FullStack Developer" twitter="@zearkiatos" avatarUrl="https://s.gravatar.com/avatar/db9770705c9ea98b2185ea09179d588f?s=80" />
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm />
                         </div>
                     </div>
                 </div>
