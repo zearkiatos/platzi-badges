17a40be044e832bb06d680bd0aef8c71983d6404
Pedro Capriles <caprilespe@outlook.com>
2019-10-26 21:36:16 -0300

    Our first page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById("app");
 
-ReactDOM.render(<Badge firstName="Pedro" lastName="Capriles" jobTitle="FullStack Developer" twitter="@zearkiatos" avatarUrl="https://s.gravatar.com/avatar/db9770705c9ea98b2185ea09179d588f?s=80" />, container);
+// ReactDOM.render(<Badge firstName="Pedro" lastName="Capriles" jobTitle="FullStack Developer" twitter="@zearkiatos" avatarUrl="https://s.gravatar.com/avatar/db9770705c9ea98b2185ea09179d588f?s=80" />, container);
+
+ReactDOM.render(<BadgeNew />, container);
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import '../pages/styles/badgeNew/BadgeNew.css';
+import Badge from '../components/Badge';
+import header from '../assets/images/badge-header.svg';
+class BadgeNew extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo" />
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                        <Badge firstName="Pedro" lastName="Capriles" jobTitle="FullStack Developer" twitter="@zearkiatos" avatarUrl="https://s.gravatar.com/avatar/db9770705c9ea98b2185ea09179d588f?s=80" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BadgeNew;
