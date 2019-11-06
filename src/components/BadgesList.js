03350faebd8665f5a8786baf5b837d1bdf8989f2
Pedro Capriles <caprilespe@outlook.com>
2019-11-05 22:13:36 -0300

    Data request (GET)

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './styles/badgeList/BadgeList.css';
+import {Link} from 'react-router-dom';
 class BadgesList extends React.Component {
     render() {
+
+        if(this.props.badges.length == 0 ){
+            return (
+                <div>
+                    <h3>No encontramos ningún badge</h3>
+                    <Link to="/badges/new" className="btn btn-primary">
+                        Create new badge
+                    </Link>
+                </div>
+            )
+        }
         return (
             <ul className="list-unstyled">
                 {
